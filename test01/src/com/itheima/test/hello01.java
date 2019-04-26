c0ad8d311ca9a2d3f71811ff681e044a1f58e9a5
sunyangyang <15188340690@139.com>
2019-04-26 20:40:16 +0800

    添加注解和定义i值并打印输出

--- test01/src/com/itheima/test/hello01.java
+++ test01/src/com/itheima/test/hello01.java
@@ -2,6 +2,11 @@ package com.itheima.test;
 
 public class hello01 {
     public static void main(String[] args) {
+        //输出helloworld
         System.out.println("hello world");
+    }
+    public  void test(){
+        int i = 10;
+        System.out.println(i);
     }
 }
